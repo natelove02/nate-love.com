33787fab26ca64cca43c07ccd458ff5e6cd36e5b
natelove02 <natelove02@gmail.com>
2024-11-06 17:07:39 -0600

    Fixed Commit 11/6/24

---
+++ src/Pages/Home/AboutMe.jsx
@@ -0,0 +1,18 @@
+export default function AboutMe(){
+    return(
+        <section id="AboutMe" className="about--section">
+            <div className="about--section--img">
+                <img src="./img/about-me.jpeg" alt="AboutMe" />
+            </div>
+            <div className="hero--section--content--box about--section--box">
+                <div className="hero--section--content">
+                    <p className="section--title"></p>
+                    <h1 className="skills-section--heading"> About Me</h1>
+                    <p className="hero--section-description"> I’m a driven professional with a strong background in leadership and project management, currently transitioning from the military into the engineering field. With experience in managing complex operations and spearheading special projects, I’ve also developed technical skills in programming, embedded systems, and web development. I hold a secret clearance and am excited to apply my problem-solving abilities in a new engineering career.</p>
+                    <p className="hero--section-description"> In my free time, I enjoy building machine learning models to predict NFL games and working on our family website that catalogs our many family recipes. I love to run, play chess, and hike or climb outdoors as much as possible.</p>
+                </div>
+            </div>
+        </section>
+    )
+
+}
